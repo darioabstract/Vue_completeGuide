ddab9f7e7b1330c38504160502051a18afb0c1ed
Dario Milani <dario.milani@abstract.it>
2026-02-11 12:22:39 +0100

    working with datain the Vue app

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: "Finish the course and learn Vue!",
+            vueLink: "https://vuejs.org/"
+        };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!'
+            }
+            return 'Master Vue!'
+        }
+    }
+});
+
+app.mount(`#user-goal`)
