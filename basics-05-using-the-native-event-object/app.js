ecebedce044dba400014c5117281b49f0013c202
Dario Milani <dario.milani@abstract.it>
2026-02-12 16:08:49 +0100

    added starting code for assignment 3

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      surname: '',
+      // fullname: ''
     };
   },
   methods: {
@@ -19,7 +21,48 @@ const app = Vue.createApp({
 
     resetInput() {
       this.name = ''
+    },
+    outputFullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + this.surname;
     }
+  },
+  computed: {
+
+    fullname() {
+      console.log('Running again..');
+      if (this.name === '') {
+        return ''
+      }
+      return this.name + ' ' + this.surname
+    }
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+
+        }, 2000)
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.surname
+    //   }
+    // },
+    // surname(value) {
+    //   if (surname === '') {
+    //     this.fullname === '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.surname
+    //   }
+    // }
   }
 });
 
