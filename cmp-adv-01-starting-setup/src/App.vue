a31dbba2ef43efc8bafc4b4d7d955e0200adacd1
Dario Milani <dario.milani@abstract.it>
2026-03-02 11:50:33 +0100

    diving deeper with components

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <TheHeader />
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheHeader from "./components/TheHeader.vue";
+import TheHeader from "./components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader: TheHeader,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
